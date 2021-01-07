a7b2fef4ba1ad443d6c2b1dc129204a78541b652
invincible <1403415056@qq.com>
2021-01-07 11:54:38 +0800

    测试git提交代码

--- src/main/java/com/campusfavour/CampusFavourApplication.java
+++ src/main/java/com/campusfavour/CampusFavourApplication.java
@@ -3,7 +3,7 @@ package com.campusfavour;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
-* lisy的分支
+* lisy的分支  ---------test  git推送   做更改
 * */
 @SpringBootApplication
 public class CampusFavourApplication {
